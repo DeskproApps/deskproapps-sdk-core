243143ea82f6b6bd32ede14090570435169dfa95
radu <radu.helstern@deskpro.com>
2017-05-16 12:18:28 +0100

    receive xcomponent conf via app url

--- src/main/javascript/Core/Window.js
+++ src/main/javascript/Core/Window.js
@@ -1,4 +1,27 @@
+const parseDpParamsFromLocation = location => {
+
+  const dpParams = {};
+
+  const paramPrefix = 'dp.';
+  const queryParams = location.search.substring(1).split('&').map(nameAndValue => nameAndValue.split('='));
+  for (const param of queryParams) {
+    const [name, value] = param;
+    if (name.substr(0, paramPrefix.length) === paramPrefix) {
+      dpParams[name] = value;
+    }
+  }
+
+  return dpParams;
+};
+
 class WindowProxy {
+
+  constructor() {
+    this.state = {
+      dpParams: parseDpParamsFromLocation(window.location)
+    };
+  }
+
   onLoad = (cb) => {
     if (window.document.readyState === 'complete') {
       cb();
@@ -11,6 +34,8 @@ class WindowProxy {
   get xchild() {
     return window.xchild;
   }
+
+  get dpParams() { return this.state.dpParams; }
 }
 
 export const windowProxy = new WindowProxy();
--- src/main/javascript/Core/createApp.js
+++ src/main/javascript/Core/createApp.js
@@ -22,66 +22,76 @@ for (const eventName of SdkEvents.eventNames) {
   postRobotOn(eventName, event => { ResponseEventDispatcher.emit(eventName, event.data)  });
 }
 
-const xcomponentOptions = {
-  dimensions: {
-    width: '100%',
-    height: '100%'
-  },
-  props: {
-
-    widgetId: {
-      type: 'string',
-      required: true
-    },
-
-    appId: {
-      type: 'string',
-      required: true
-    },
-
-    appTitle: {
-      type: 'string',
-      required: true
-    },
-
-    appPackageName: {
-      type: 'string',
-      required: true
-    },
-
-    instanceId: {
-      type: 'string',
-      required: true
-    },
-
-    onDpMessage: {
-      type: 'function',
-      required: true
-    }
-  },
-  scrolling: false,
-  autoResize: true,
-  tag: 'ticket-sidebar', //needs to match xcomponent config on the parent
-  url: 'http://localhost', // dummy entry to bypass validation
-};
-
-const getReceivedProps = xchild => {
+const getPropsFromXchild = xchild => {
   const { appId, appTitle, appPackageName, instanceId } = xchild.props;
   return { appId, appTitle, appPackageName, instanceId };
+};
+
+/**
+ * @param dpParams
+ * @return {{dimensions: {width: string, height: string}, props: {widgetId: {type: string, required: boolean}, appId: {type: string, required: boolean}, appTitle: {type: string, required: boolean}, appPackageName: {type: string, required: boolean}, instanceId: {type: string, required: boolean}, onDpMessage: {type: string, required: boolean}}, scrolling: boolean, autoResize: boolean, tag: *, url: string}}
+ */
+const getXcomponentOptions = (dpParams) => {
+
+  return {
+    dimensions: {
+      width: '100%',
+      height: '100%'
+    },
+    props: {
+      widgetId: {
+        type: 'string',
+        required: true
+      },
+
+      appId: {
+        type: 'string',
+        required: true
+      },
+
+      appTitle: {
+        type: 'string',
+        required: true
+      },
+
+      appPackageName: {
+        type: 'string',
+        required: true
+      },
+
+      instanceId: {
+        type: 'string',
+        required: true
+      },
+
+      onDpMessage: {
+        type: 'function',
+        required: true
+      }
+    },
+    scrolling: false,
+    autoResize: true,
+    tag: dpParams['dp.xconf.tag'], //needs to match xcomponent config on the parent
+    url: 'http://localhost', // dummy entry to bypass validation
+  };
+
 };
 
 /**
  * @param {function} cb
  */
 const createApp = (cb) => {
+  const xcomponentOptions = getXcomponentOptions(windowProxy.dpParams);
   const xcomponent = create(xcomponentOptions);
+
   if (xcomponent.isChild()) {
       xcomponent.child().init().then(xchild => {
-        const receivedProps = getReceivedProps(xchild);
-        const props = { eventDispatcher: RequestEventDispatcher, ...receivedProps };
+
+        const injectedProps = getPropsFromXchild(xchild);
+        const props = { eventDispatcher: RequestEventDispatcher, ...injectedProps };
 
         const app = new App(props);
-        cb(app);
+        windowProxy.onLoad(() => cb(app));
       }).catch();
   } else if (isBrowser()) {
       windowProxy.onLoad(() => cb(app));
