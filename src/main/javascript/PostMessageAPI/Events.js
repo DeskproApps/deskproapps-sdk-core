8d814f59aeaca9e42e83663ddef069d0ddf76ab0
radu <radu.helstern@deskpro.com>
2017-06-01 17:29:29 +0100

    remove obsolete api

--- src/main/javascript/PostMessageAPI/Events.js
+++
@@ -1,5 +0,0 @@
-export const EVENT_USER_GET = 'get-user';
-
-export const eventNames = [ EVENT_USER_GET ];
-
-export const isEventName = name => eventNames.indexOf(name) !== -1;
--- src/main/javascript/PostMessageAPI/index.js
+++
@@ -1,6 +0,0 @@
-export UserAPIClient from './UserAPIClient';
-
-import * as SDKEvents from './Events';
-export { SDKEvents };
-
-export { registerRequestListeners , registerListeners } from './EventHandlers';
