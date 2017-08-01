4d2dd7367ddd3af25065f1cf61d8058cace42ca9
radu <radu.helstern@deskpro.com>
2017-08-01 13:29:37 +0100

    - add settings method to retrieve an oauth provider's  settings

--- src/main/javascript/Security/EventHandlers.js
+++ src/main/javascript/Security/EventHandlers.js
@@ -3,4 +3,5 @@ import { handleOutgoingEvent } from '../Core/EventHandler';
 
 export const registerEventHandlers = () => {
   handleOutgoingEvent(Events.EVENT_SECURITY_AUTHENTICATE_OAUTH, Events.props.EVENT_SECURITY_AUTHENTICATE_OAUTH);
+  handleOutgoingEvent(Events.EVENT_SECURITY_SETTINGS_OAUTH, Events.props.EVENT_SECURITY_SETTINGS_OAUTH);
 };
--- src/main/javascript/Security/index.js
+++ src/main/javascript/Security/index.js
@@ -6,9 +6,16 @@ export { registerEventHandlers } from './EventHandlers';
 
 import { createStateAPIClient } from '../State';
 
+/**
+ * @param eventDispatcher
+ * @param {InstanceProps} instanceProps
+ * @param {ContextProps} contextProps
+ * @return {OauthFacade}
+ */
 export const createOauthAPIClient = (eventDispatcher, instanceProps, contextProps) => {
   const stateClient = createStateAPIClient(eventDispatcher, instanceProps, contextProps);
   const setState = stateClient.setAppState.bind(stateClient);
 
-  return new OauthFacade(eventDispatcher, setState);
+  const props = { ...instanceProps.toJS(), ...contextProps.toJS()};
+  return new OauthFacade(eventDispatcher, setState, props);
 };
