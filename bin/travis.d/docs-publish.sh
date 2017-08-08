294d688bda8d88a4c19e8deeb2a2eeb8b98e2f7d
radu <radu.helstern@deskpro.com>
2017-08-08 12:53:27 +0100

    testing docs deployment

--- bin/travis.d/docs-publish.sh
+++ bin/travis.d/docs-publish.sh
@@ -6,7 +6,7 @@ git config user.name "Travis CI"
 git config user.email "${COMMIT_AUTHOR_EMAIL}"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
+if git diff --quiet -- docs/reference; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
