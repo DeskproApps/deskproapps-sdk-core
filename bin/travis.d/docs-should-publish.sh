13520d6951501c2ab47bfc70aa5a28f358009aaa
radu <radu.helstern@deskpro.com>
2017-08-08 14:28:45 +0100

    publish docs on push to master

--- bin/travis.d/docs-should-publish.sh
+++ bin/travis.d/docs-should-publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SOURCE_BRANCH="add-documentation"
+SOURCE_BRANCH="master"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "{$TRAVIS_PULL_REQUEST}" != "false" -o "${TRAVIS_BRANCH}" != "${SOURCE_BRANCH}" ]; then
     exit 0
