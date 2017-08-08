df0377cf1ab9b8f7521a5e8ebac3cc765688f7d8
radu <radu.helstern@deskpro.com>
2017-08-08 13:52:54 +0100

    testing docs deployment

--- bin/travis.d/docs-publish.sh
+++ bin/travis.d/docs-publish.sh
@@ -6,10 +6,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$(cd ${DIR}/../../ && pwd)"
 DEPLOY_KEY_ROOT="${PROJECT_ROOT}/src/travis"
 
-echo deploy key label : ${DEPLOY_KEY_LABEL} ${DEPLOY_KEY_ROOT}
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 
 git config user.name "Travis CI"
 git config user.email "${COMMIT_AUTHOR_EMAIL}"
+git config --global push.default matching
 git config core.autocrlf false
 git config core.safecrlf false
 
@@ -30,12 +31,12 @@ ENCRYPTED_KEY_VAR="encrypted_${DEPLOY_KEY_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${DEPLOY_KEY_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-## openssl aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in src/travis/deploy-key.enc -out src/travis/deploy-key -d
-openssl aes-256-cbc -K $encrypted_3778a32db008_key -iv $encrypted_3778a32db008_iv -in deploy-key.enc -out ${DEPLOY_KEY_ROOT}/deploy-key -d
+openssl aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in src/travis/deploy-key.enc -out src/travis/deploy-key -d
+## openssl aes-256-cbc -K $encrypted_3778a32db008_key -iv $encrypted_3778a32db008_iv -in deploy-key.enc -out ${DEPLOY_KEY_ROOT}/deploy-key -d
 
 chmod 600 src/travis/deploy-key
 eval `ssh-agent -s`
 ssh-add src/travis/deploy-key
 
 # Now that we're all set up, we can push.
-git push origin
+git push ${SSH_REPO}
