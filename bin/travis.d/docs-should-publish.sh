2fbf041e48a1967e2c2d88902fea236092bbd5a8
radu <radu.helstern@deskpro.com>
2017-08-09 17:41:14 +0100

    add log statements to mark if build will generate docs

--- bin/travis.d/docs-should-publish.sh
+++ bin/travis.d/docs-should-publish.sh
@@ -3,7 +3,9 @@
 SOURCE_BRANCH="master"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "{$TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "${SOURCE_BRANCH}" ]; then
+    echo "DOCS: documentation WILL be published"
     exit 0
 else
+    echo "DOCS: documentation WILL NOT be published"
     exit 1
 fi
